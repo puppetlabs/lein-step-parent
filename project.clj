370db0f444f2dcf25045804abd934175c1fdb87a
Chris Price <chris@puppetlabs.com>
2016-09-29 16:30:30 -0700

    (TK-408) Add deploy-repositories to deploy to clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/puppetlabs/lein-step-parent"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]]
+)
